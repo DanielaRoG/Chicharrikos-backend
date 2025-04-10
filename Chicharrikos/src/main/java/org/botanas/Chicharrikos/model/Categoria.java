b12d991bffbf814ffe8f073b130319ba7927e5b2
Denisse Hernández <denisse.hernandezvazquez16@gmail.com>
2025-04-10 17:10:09 -0600

    Creacion de producto, controller y repository, tambien de categoria

--- Chicharrikos/src/main/java/org/botanas/Chicharrikos/model/Categoria.java
+++ Chicharrikos/src/main/java/org/botanas/Chicharrikos/model/Categoria.java
@@ -1,4 +1,48 @@
 package org.botanas.Chicharrikos.model;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+
+    @Id
+    private Long categoria_id;
+
+    private String nombre;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<org.botanas.Chicharrikos.model.Producto> productos;
+
+    public Categoria() {}
+
+    public Categoria(Long categoria_id, String nombre) {
+        this.categoria_id = categoria_id;
+        this.nombre = nombre;
+    }
+
+    public Long getCategoria_id() {
+        return categoria_id;
+    }
+
+    public void setCategoria_id(Long categoria_id) {
+        this.categoria_id = categoria_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 }
--- Chicharrikos/src/main/java/org/botanas/Chicharrikos/model/Producto.java
+++ Chicharrikos/src/main/java/org/botanas/Chicharrikos/model/Producto.java
@@ -1,4 +1,100 @@
 package org.botanas.Chicharrikos.model;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table (name =  "producto")
 public class Producto {
-}
+
+    @Id
+    private Long idproducto;
+    private String descripcion;
+    private Double precio;
+    private Long existencia;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id") // se relaciona con la columna categoria_id de la tabla producto
+    private Categoria categoria;
+
+    // constructor vacio para JBA
+    public Producto(){
+    }
+
+    //Contructor
+    public Producto(Long idproducto, String descripcion, Double precio, Long existencia, Long categoria_id) {
+        this.idproducto = idproducto;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.existencia = existencia;
+        this.categoria = categoria;
+    }
+
+    // Getter and Setter
+
+    public Long getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(Long idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Long getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(Long existencia) {
+        this.existencia = existencia;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    //Tostring
+
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "idproducto=" + idproducto +
+                ", descripcion='" + descripcion + '\'' +
+                ", precio=" + precio +
+                ", existencia=" + existencia +
+                ", categoria=" + categoria +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Producto producto)) return false;
+        return Objects.equals(idproducto, producto.idproducto) && Objects.equals(descripcion, producto.descripcion) && Objects.equals(precio, producto.precio) && Objects.equals(existencia, producto.existencia) && Objects.equals(categoria, producto.categoria);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idproducto, descripcion, precio, existencia, categoria);
+    }
+}// class producto
