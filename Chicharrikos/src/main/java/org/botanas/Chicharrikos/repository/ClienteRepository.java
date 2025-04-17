4ea8a15a107f0d76af3e24d3b331f5558edf41ac
Jenny <jmontanor1800@alumno.ipn.mx>
2025-04-16 22:12:22 -0600

    Union de denisse, dany e itzel

--- Chicharrikos/src/main/java/org/botanas/Chicharrikos/repository/ClienteRepository.java
+++ Chicharrikos/src/main/java/org/botanas/Chicharrikos/repository/ClienteRepository.java
@@ -3,8 +3,9 @@ package org.botanas.Chicharrikos.repository;
 import org.botanas.Chicharrikos.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {}
-
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Cliente findByCorreo(String correo);
+}
 
 
 
