44cb12e65348c9deea5a101756c70a17f21f81b0
Ximenagarciau <ximenarodman9124@gmail.com>
2025-04-23 20:36:35 -0600

    actualización de mi rama

--- Chicharrikos/src/main/java/org/botanas/Chicharrikos/model/Categoria.java
+++ Chicharrikos/src/main/java/org/botanas/Chicharrikos/model/Categoria.java
@@ -26,26 +26,32 @@ public class Categoria {
     }
 
     public Long getCategoria_id() {
+
         return categoria_id;
     }
 
     public void setCategoria_id(Long categoria_id) {
+
         this.categoria_id = categoria_id;
     }
 
     public String getNombre() {
+
         return nombre;
     }
 
     public void setNombre(String nombre) {
+
         this.nombre = nombre;
     }
 
     public List<Producto> getProductos() {
+
         return productos;
     }
 
     public void setProductos(List<Producto> productos) {
+
         this.productos = productos;
     }
 }
